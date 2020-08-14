87a544276fa0d656967a8d8f60ad6e45e6f78ca7
smilesandtea <smilesandcode@gmail.com>
2020-08-14 17:07:45 -0400

    adding site.

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+  $title = 'Top 5 Learning Tips';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8>
+  	<title><?php echo $title; ?></title>
+  	<link href="css/style.css" rel="stylesheet" />
+  </head>
+  
+  <body>
+    <div class="wrap">
+        <div class="sidebar">
+          <?php echo include 'inc/learning_quotes.html'; ?>
+        </div>
+        <div class="main">
+          <h1><?php echo $title; ?></h1>
+    
+          <?php echo include 'inc/learning_tips.html'; ?>
+
+        </div>
+        <div class="footer">Practice Completed: <?php echo date('F j, Y'); ?></div>
+    </div>
+  </body>
+</html>
